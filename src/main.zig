f0c1d2fd88e14dc5b1961205043a0718d1960dcb
Mauricio Rojas <mavro@MacBook-Pro-de-Mauricio.local>
2022-06-11 20:44:08 -0400

    chore: add basic web server

---
+++ src/main.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+const net = std.net;
+const StreamServer = net.StreamServer;
+const Address = net.Address;
+const stdout = std.io.getStdOut().writer();
+
+pub fn main() anyerror!void {
+    var stream_server = StreamServer.init(.{});
+    defer stream_server.close();
+    const address = try Address.resolveIp("127.0.0.1", 8080);
+    try stream_server.listen(address);
+
+    while(true) {
+        try stdout.print("Server is running on port, {s}!\n", .{"8080"});
+        const connection = try stream_server.accept();
+        try connection.stream.writer().print("Hello world from zig", .{});
+        connection.stream.close();
+    }
+}
